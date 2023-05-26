d7080983edfa9d00fff919e9db1d7d104e74d59a
Aran <aransjannson@gmail.com>
2023-05-26 01:34:51 +0100

    Removed redundent file

---
+++ Basic_Java_Tasks/src/task5/task5.java
@@ -0,0 +1,42 @@
+package task5;
+
+public class task5 {
+
+	 /**
+	   * Loops and Arrays
+	   *
+	   * @author Aran Jannson
+	   */
+	
+	//Local Variable
+	private int[] array;
+	
+	/**
+	 * Default Constructor
+	 */
+	
+	public task5() {
+		super();
+	}
+	
+	
+	/**
+	 * Set the size of the array to '5' and add each value in the Data Set into the array
+	 * Then using a Loop go through the array and return the value in position '3'
+	 * 
+	 * DataSet: 1, 4, 7, 2, 6
+	 * 
+	 * Note: Arrays start from 0
+	 * 
+	 * @return Integer, 2
+	 */
+
+	
+	public int arrayIteration() {
+		//TODO: Add code
+		
+		return 0;
+	}
+	
+	
+}
